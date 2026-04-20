91fb1e3d2847e2a4b071bf345ac9a8e5e9422bed
joaovictorz10 <jmarquito@sga.pucminas.br>
2026-04-20 18:34:56 -0300

    fixing clientes

--- codigo/GestaoAluguelVeiculos/src/main/java/br/gestao/repository/ClienteRepository.java
+++ codigo/GestaoAluguelVeiculos/src/main/java/br/gestao/repository/ClienteRepository.java
@@ -2,16 +2,14 @@ package br.gestao.repository;
 
 import io.micronaut.data.annotation.Repository;
 import io.micronaut.data.annotation.Query;
-import io.micronaut.data.repository.CrudRepository;
+import io.micronaut.data.jpa.repository.JpaRepository;
 import br.gestao.model.Cliente;
 import java.util.Optional;
 import java.util.List;
 
 @Repository
-public interface ClienteRepository extends CrudRepository<Cliente, Long> {
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
     
     @Query("SELECT c FROM Cliente c WHERE c.usuario.id = :usuarioId")
     Optional<Cliente> findByUsuarioId(Long usuarioId);
-    
-    List<Cliente> findAll();
 }
